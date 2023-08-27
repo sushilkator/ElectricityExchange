e1d91064c15c5768e394236f1e0238c9cb15de54
Sushil Kumar <sushilkator@gmail.com>
2023-08-27 11:39:25 +0530

    Update util.js

--- util.js
+++ util.js
@@ -1,31 +1,42 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const twitList = require('./searchTweet');
 
-module.exports = {
-    extractProjectInfo: (data)=>{
-       return {
-            projectName: data.name,
-            projectFullName : data.full_name,
-            projectDescription: data.description,
-            tweets: []
-        }
-    },
-    extractTweetInfo : (data)=>{
-       let list =  data.statuses.map(d => {
-            return { name: d.user.name, text: d.text, date: d.created_at };
-        });
-        return list;
-    },
-    writeData : (data)=>{
-        fs.writeFile("tweets.json", data, (err)=> {
-            if(err) {
-                 console.log("Some issue with file write");
-            }
-            console.log("To see final result see 'tweets.json' file !");
-        }); 
-    },
-    tweets : (list)=>{
-        let data = list.map(d=>{return twitList(d.name);})
-        return Promise.all(data);
+const extractProjectInfo = ({ name, full_name, description }) => ({
+    projectName: name,
+    projectFullName: full_name,
+    projectDescription: description,
+    tweets: []
+});
+
+const extractTweetInfo = (data) => 
+    data.statuses.map(({ user, text, created_at }) => ({
+        name: user.name,
+        text,
+        date: created_at
+    }));
+
+const writeData = async (data) => {
+    try {
+        await fs.writeFile("tweets.json", data);
+        console.log("To see final results, check 'tweets.json' file!");
+    } catch (err) {
+        console.error("Error writing to 'tweets.json'", err);
     }
-}
+};
+
+const tweets = async (list) => {
+    const promises = list.map(project => twitList(project.name));
+    try {
+        return await Promise.all(promises);
+    } catch (err) {
+        console.error("Error fetching tweets for the projects", err);
+        throw err;
+    }
+};
+
+module.exports = {
+    extractProjectInfo,
+    extractTweetInfo,
+    writeData,
+    tweets
+};
