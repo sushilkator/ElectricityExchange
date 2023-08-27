35b187768bb7a6ac539c459fe1e05c680a5b7c4a
Sushil Kumar <sushilkator@gmail.com>
2023-08-27 11:38:04 +0530

    Update searchTweet.js

--- searchTweet.js
+++ searchTweet.js
@@ -1,13 +1,16 @@
 const Twitter = require('twitter');
-const twitterConfig = require("./twitterConfig");
+const twitterConfig = require('./twitterConfig');
+
 const client = new Twitter(twitterConfig);
-const gitTweetList = async(q) => {
-    return new Promise((resolve, reject) => {
-        client.get('search/tweets', { q: q }, (error, tweets, response) => {
-            if (error)
-                reject(error)
-            resolve(tweets)
-        });
-    });
-}
+
+const gitTweetList = async (query) => {
+    try {
+        const tweets = await client.get('search/tweets', { q: query });
+        return tweets;
+    } catch (error) {
+        console.error(`Error fetching tweets for query: ${query}`, error.message);
+        throw error;
+    }
+};
+
 module.exports = gitTweetList;
