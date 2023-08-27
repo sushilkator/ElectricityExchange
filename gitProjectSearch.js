58558082e38d86394d3a135742baa0ee3ea12787
Sushil Kumar <sushilkator@gmail.com>
2023-08-27 11:37:08 +0530

    Update gitProjectSearch.js

--- gitProjectSearch.js
+++ gitProjectSearch.js
@@ -1,14 +1,26 @@
 const rp = require('request-promise');
-const gitProjectList = async (name)=>{
-    const options = {
-        method: 'GET',  
-        url: 'https://api.github.com/search/repositories',
-        qs: { q: name},
-        json: true,
-        headers: {'user-agent': 'node.js'}
-    };    
-    return rp(options);   
-}
 
+const GITHUB_API_BASE_URL = 'https://api.github.com';
+
+const gitProjectList = async (name) => {
+    const options = {
+        method: 'GET',
+        uri: `${GITHUB_API_BASE_URL}/search/repositories`,
+        qs: {
+            q: name
+        },
+        json: true,
+        headers: {
+            'user-agent': 'node.js'
+        }
+    };
+
+    try {
+        return await rp(options);
+    } catch (error) {
+        console.error(`Error fetching repositories for keyword: ${name}`, error.message);
+        throw error;
+    }
+};
 
 module.exports = gitProjectList;
